a96113bc0ab2572ece15ddc8c9dc3966c33a1564
Ken Wilcox <wilcoxk@gmail.com>
2016-03-21 05:17:28 -0600

    wrapped the randomness in a struct

--- Whack-a-Penguin/Helper.swift
+++ Whack-a-Penguin/Helper.swift
@@ -9,36 +9,33 @@
 import Foundation
 import UIKit
 
-func RandomInt(min min: Int, max: Int) -> Int {
-  if max < min { return min }
-  return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
-}
-
-func RandomFloat() -> Float {
-  return Float(arc4random()) /  Float(UInt32.max)
-}
-
-func RandomFloat(min min: Float, max: Float) -> Float {
-  return (Float(arc4random()) / Float(UInt32.max)) * (max - min) + min
-}
-
-func RandomDouble(min min: Double, max: Double) -> Double {
-  return (Double(arc4random()) / Double(UInt32.max)) * (max - min) + min
-}
-
-func RandomCGFloat() -> CGFloat {
-  return CGFloat(RandomFloat())
-}
-
-func RandomCGFloat(min min: Float, max: Float) -> CGFloat {
-  return CGFloat(RandomFloat(min: min, max: max))
-}
-
-func RandomColor() -> UIColor {
-  return UIColor(red: RandomCGFloat(), green: RandomCGFloat(), blue: RandomCGFloat(), alpha: 1)
-}
-
-func RunAfterDelay(delay: NSTimeInterval, block: dispatch_block_t) {
-  let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
-  dispatch_after(time, dispatch_get_main_queue(), block)
+struct Random {
+  static func int(min min: Int, max: Int) -> Int {
+    if max < min { return min }
+    return Int(arc4random_uniform(UInt32((max - min) + 1))) + min
+  }
+  
+  static func float() -> Float {
+    return Float(arc4random()) /  Float(UInt32.max)
+  }
+  
+  static func float(min min: Float, max: Float) -> Float {
+    return (Float(arc4random()) / Float(UInt32.max)) * (max - min) + min
+  }
+  
+  static func double(min min: Double, max: Double) -> Double {
+    return (Double(arc4random()) / Double(UInt32.max)) * (max - min) + min
+  }
+  
+  static func cgFloat() -> CGFloat {
+    return CGFloat(Random.float())
+  }
+  
+  static func cgFloat(min min: Float, max: Float) -> CGFloat {
+    return CGFloat(Random.float(min: min, max: max))
+  }
+  
+  static func color() -> UIColor {
+    return UIColor(red: Random.cgFloat(), green: Random.cgFloat(), blue: Random.cgFloat(), alpha: 1)
+  }
 }
