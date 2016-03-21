31ba3006827eff39618c82d513d0d2f6ce20f941
Ken Wilcox <wilcoxk@gmail.com>
2016-03-21 04:54:53 -0600

    Added some assets, set up the background image

--- Whack-a-Penguin/GameViewController.swift
+++ Whack-a-Penguin/GameViewController.swift
@@ -35,11 +35,9 @@ class GameViewController: UIViewController {
   }
   
   override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-    if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-      return .AllButUpsideDown
-    } else {
-      return .All
-    }
+//    if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+//      return .AllButUpsideDown
+      return .Landscape
   }
   
   override func didReceiveMemoryWarning() {
