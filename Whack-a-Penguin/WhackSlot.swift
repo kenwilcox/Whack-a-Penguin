7ecf0a879a92b6d91901ff6de38c208bf4ffbcef
Ken Wilcox <wilcoxk@gmail.com>
2016-03-21 05:14:06 -0600

    Added penguins and a crop mask to hide behind

--- Whack-a-Penguin/WhackSlot.swift
+++ Whack-a-Penguin/WhackSlot.swift
@@ -11,11 +11,25 @@ import SpriteKit
 
 class WhackSlot: SKNode {
   
+  var charNode: SKSpriteNode!
+  
   func configureAtPosition(pos: CGPoint) {
     position = pos
     
     let sprite = SKSpriteNode(imageNamed: "whackHole")
     addChild(sprite)
+    
+    let cropNode = SKCropNode()
+    cropNode.position = CGPoint(x: 0, y: 15)
+    cropNode.zPosition = 1
+    cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+    
+    charNode = SKSpriteNode(imageNamed: "penguinGood")
+    charNode.position = CGPoint(x: 0, y: -90)
+    charNode.name = "character"
+    cropNode.addChild(charNode)
+    
+    addChild(cropNode)
   }
   
 }
